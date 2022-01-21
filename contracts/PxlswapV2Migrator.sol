ad4f58f3fa12ecbdf6a7517e6f8997771324c6e8
RadSalty <mbsulcs@gmail.com>
2022-01-21 14:06:55 +0800

    Cleaning names

--- contracts/PxlswapV2Migrator.sol
+++ contracts/PxlswapV2Migrator.sol
@@ -1,36 +1,39 @@
 pragma solidity =0.6.6;
 
-import '@uniswap/lib/contracts/libraries/TransferHelper.sol';
+import '@RadSalty/solidity-lib/contracts/libraries/TransferHelper.sol';
 
-import './interfaces/IUniswapV2Migrator.sol';
-import './interfaces/V1/IUniswapV1Factory.sol';
-import './interfaces/V1/IUniswapV1Exchange.sol';
-import './interfaces/IUniswapV2Router01.sol';
+import './interfaces/IPxlswapV2Migrator.sol';
+import './interfaces/V1/IPxlswapV1Factory.sol';
+import './interfaces/V1/IPxlswapV1Exchange.sol';
+import './interfaces/IPxlswapV2Router01.sol';
 import './interfaces/IERC20.sol';
 
-contract UniswapV2Migrator is IUniswapV2Migrator {
-    IUniswapV1Factory immutable factoryV1;
-    IUniswapV2Router01 immutable router;
+contract PxlswapV2Migrator is IPxlswapV2Migrator {
+    IPxlswapV1Factory immutable factoryV1;
+    IPxlswapV2Router01 immutable router;
 
     constructor(address _factoryV1, address _router) public {
-        factoryV1 = IUniswapV1Factory(_factoryV1);
-        router = IUniswapV2Router01(_router);
+        factoryV1 = IPxlswapV1Factory(_factoryV1);
+        router = IPxlswapV2Router01(_router);
     }
 
     // needs to accept ETH from any v1 exchange and the router. ideally this could be enforced, as in the router,
     // but it's not possible because it requires a call to the v1 factory, which takes too much gas
     receive() external payable {}
 
-    function migrate(address token, uint amountTokenMin, uint amountETHMin, address to, uint deadline)
-        external
-        override
-    {
-        IUniswapV1Exchange exchangeV1 = IUniswapV1Exchange(factoryV1.getExchange(token));
-        uint liquidityV1 = exchangeV1.balanceOf(msg.sender);
+    function migrate(
+        address token,
+        uint256 amountTokenMin,
+        uint256 amountETHMin,
+        address to,
+        uint256 deadline
+    ) external override {
+        IPxlswapV1Exchange exchangeV1 = IPxlswapV1Exchange(factoryV1.getExchange(token));
+        uint256 liquidityV1 = exchangeV1.balanceOf(msg.sender);
         require(exchangeV1.transferFrom(msg.sender, address(this), liquidityV1), 'TRANSFER_FROM_FAILED');
-        (uint amountETHV1, uint amountTokenV1) = exchangeV1.removeLiquidity(liquidityV1, 1, 1, uint(-1));
+        (uint256 amountETHV1, uint256 amountTokenV1) = exchangeV1.removeLiquidity(liquidityV1, 1, 1, uint256(-1));
         TransferHelper.safeApprove(token, address(router), amountTokenV1);
-        (uint amountTokenV2, uint amountETHV2,) = router.addLiquidityETH{value: amountETHV1}(
+        (uint256 amountTokenV2, uint256 amountETHV2, ) = router.addLiquidityETH{value: amountETHV1}(
             token,
             amountTokenV1,
             amountTokenMin,
